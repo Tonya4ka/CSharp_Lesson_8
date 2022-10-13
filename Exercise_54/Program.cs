cb220a354eefbd41a43f1d0aac0bcc821f1086be
tonyatrushkova <antonina.trushkova@gmail.com>
2022-10-13 19:33:07 +0300

    Add Exercise_54 - descending sorting

---
+++ Exercise_54/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива
+
+Console.Clear();
+int[,] matrix = new int[5,5];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+SortDescendingArray(matrix);
+PrintArray(matrix);
+
+void SortDescendingArray(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+void FillArray(int[,] matrix)
+{
+    Random generator = new Random();
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = generator.Next(0,100);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine("");
+    }
+}

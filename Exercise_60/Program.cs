3991a73a601d3f4728a1064926cac6d0e6454c42
tonyatrushkova <antonina.trushkova@gmail.com>
2022-10-14 12:15:58 +0300

    Add Exercise_60 - matrix with index

---
+++ Exercise_60/Program.cs
@@ -0,0 +1,35 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
+Console.Clear();
+int[,,] array = new int[2,2,2];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Random gen = new Random();
+                array[i, j, k] = gen.Next(10, 100);
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+               Console.Write($"{array[i,j,k]}({i},{j},{k}) ");
+            } 
+            Console.WriteLine("");
+        }
+    }
+}

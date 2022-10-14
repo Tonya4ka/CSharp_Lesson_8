376eb8d6c0fae4332ca2694ab3a9e8eee98ba664
tonyatrushkova <antonina.trushkova@gmail.com>
2022-10-14 13:04:20 +0300

    Add Exercise_62 - spiral matrix

---
+++ Exercise_62/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4
+Console.Clear();
+int[,] matrix = new int[4,4];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+PrintArray(matrix);
+
+void PrintArray(int[,] matrix)
+{
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+            if (matrix[i,j] / 10 <= 0)
+            Console.Write($" {matrix[i,j]} ");
+
+            else Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
